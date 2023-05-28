71d2ea10663198872f7377d548c56711fb1862d4
Alexandr Zhdanko <zhdko@yandex.ru>
2023-05-28 22:01:14 +0400

    fix: назначение поле name обязательным при регистрации

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -7,7 +7,7 @@ const {
 
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   })
